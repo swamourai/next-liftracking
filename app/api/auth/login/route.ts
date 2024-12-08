70e703e9e66396f4a81b59ca134e489df92a793b
Swamouraï <swamourai@gmail.com>
2024-12-08 21:05:02 +0100

    Adapt api login route.ts

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,33 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import prisma from "@/src/lib/prisma";
 import { loginSchema } from "@/src/schemas/userSchema";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      const data = loginSchema.parse(req.body);
+export async function POST(req: Request) {
+  try {
+    const data = loginSchema.parse(req.body);
 
-      // Vérifier si l'utilisateur existe
-      const user = await prisma.user.findUnique({
-        where: { email: data.email },
-      });
+    // Vérifier si l'utilisateur existe
+    const user = await prisma.user.findUnique({
+      where: { email: data.email },
+    });
 
-      if (!user || !(await bcrypt.compare(data.password, user.password))) {
-        return res.status(401).json({ error: "Invalid email or password" });
-      }
-
-      // session here
-
-      return res.status(200).json({ message: "Login successful", user });
-    } catch (err: unknown) {
-        if (err instanceof Error) {
-          return res.status(400).json({ error: err.message });
-        }
-        // Pour les cas où l'erreur n'est pas une instance d'Error
-        return res.status(400).json({ error: "An unexpected error occurred" });
+    if (!user || !(await bcrypt.compare(data.password, user.password))) {
+      return NextResponse.json({ error: "Invalid email or password" }, { status: 401 });
     }
-  }
 
-  res.status(405).json({ error: "Method not allowed" });
+    // session here
+
+    return NextResponse.json({ message: "Login successful", user }, { status: 200 });
+  } catch (err: unknown) {
+      if (err instanceof Error) {
+        return NextResponse.json({ error: err.message }, { status: 400 });
+      }
+      // Pour les cas où l'erreur n'est pas une instance d'Error
+      return NextResponse.json({ error:  "An unexpected error occurred" }, { status: 400 });
+  }
 }
